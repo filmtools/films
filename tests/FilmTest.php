535fc0f43b892d6e488c6d49ff238fc33e8be0a7
Carsten Witt <412560+tomkyle@users.noreply.github.com>
2018-01-03 11:22:04 +0100

    Updated test class

--- tests/FilmTest.php
+++ tests/FilmTest.php
@@ -11,56 +11,82 @@ class FilmTest extends TestCase
     /**
      * @dataProvider provideManufacturerAndName
      */
-    public function testSimpleInstantiation( $manufacturer, $name, $asa )
+    public function testSimpleInstantiation( $manufacturer, $name, $asa, $expected_name )
     {
-        $sut = new Film( $manufacturer, $name, $asa);
+        $sut = new Film;
+        $sut->setManufacturer( $manufacturer );
+        $sut->setName( $name );
+        $sut->setAsa( $asa );
+
         $this->assertInstanceOf( FilmInterface::class, $sut );
-        $this->assertNotEmpty( (string) $sut );
+
+        $string_sut = (string) $sut;
+        $this->assertNotEmpty( $string_sut );
+
+        $this->assertEquals($string_sut, $sut->__toString());
+        $this->assertEquals($expected_name, $sut->__toString());
     }
 
     /**
      * @dataProvider provideManufacturerAndName
      */
-    public function testNameInterceptors( $manufacturer, $name, $asa )
+    public function testNameInterceptors( $manufacturer, $name, $asa, $expected_name )
     {
-        $sut = new Film( $manufacturer, $name, $asa);
+        $sut = new Film;
+        $sut->setManufacturer( $manufacturer );
+        $sut->setName( $name );
+        $sut->setAsa( $asa );
+
         $new_name = "foo";
         $this->assertNotEquals( $sut->getName(), $new_name);
         $sut->setName( $new_name);
         $this->assertEquals( $sut->getName(), $new_name);
+
+        $this->assertNotEquals( $sut->__toString(), $expected_name);
     }
 
     /**
      * @dataProvider provideManufacturerAndName
      */
-    public function testManufacturerInterceptors( $manufacturer, $name, $asa )
+    public function testManufacturerInterceptors( $manufacturer, $name, $asa, $expected_name )
     {
-        $sut = new Film( $manufacturer, $name);
+        $sut = new Film;
+        $sut->setManufacturer( $manufacturer );
+        $sut->setName( $name );
+
         $new_manufacturer = "foo";
         $this->assertNotEquals( $sut->getManufacturer(), $new_manufacturer);
         $sut->setManufacturer( $new_manufacturer);
         $this->assertEquals( $sut->getManufacturer(), $new_manufacturer);
+
+        $this->assertNotEquals( $sut->__toString(), $expected_name);
     }
 
     /**
      * @dataProvider provideManufacturerAndName
      */
-    public function testAsaInterceptors( $manufacturer, $name, $asa )
+    public function testAsaInterceptors( $manufacturer, $name, $asa, $expected_name )
     {
-        $sut = new Film( $manufacturer, $name);
+        $sut = new Film;
+        $sut->setManufacturer( $manufacturer );
+        $sut->setName( $name );
+
         $new_asa = 1;
         $this->assertNotEquals( $sut->getAsa(), $new_asa);
         $sut->setAsa( $new_asa);
         $this->assertEquals( $sut->getAsa(), $new_asa);
+
+        $this->assertNotEquals( $sut->__toString(), $expected_name);
     }
 
 
     public function provideManufacturerAndName()
     {
         return array(
-            [ "Ilford", "HP5+", 400],
-            [ "Ilford", null, null ],
-            [ null, "FP4+", 125 ]
+            array( "Ilford", "HP5+", 400, "Ilford HP5+ 400" ),
+            [ "Ilford", null, null, "Ilford" ],
+            [ null, "FP4+", 125, "FP4+ 125" ],
+            [ "Kentmere", null, 400, "Kentmere 400" ]
         );
     }
 }
